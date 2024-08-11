7c68090895fbf08aa666294dd6defb914a795faf
Juan Sapién <juansapienflores@gmail.com>
2024-08-10 21:51:59 -0700

    feat: add app theme

---
+++ lib/config/theme/app_theme.dart
@@ -0,0 +1,13 @@
+
+
+import 'package:flutter/material.dart';
+
+class AppTheme {
+
+  ThemeData getTheme() => ThemeData(
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    colorSchemeSeed: const Color.fromARGB(255, 139, 198, 250)
+  );
+
+}
